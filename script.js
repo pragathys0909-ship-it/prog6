7668e8e23d4f4b05c478183381845de31c22f1d9
pragathys0909-ship-it <pragathys0909@gmail.com>
2026-03-11 13:03:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+Script.js
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    alert("Registration Successful!\nThank you for registering.");
+});
+
+
